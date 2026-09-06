2a3e0b2138cf6cf0caf5bf546ea71574a8a335f4
Samueru-sama <xdglawyer@outlook.com>
2026-09-05 23:05:12 -0400

    test

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm glu openal pipewire-audio pipewire-jack
+pacman -Syu --noconfirm glu openal
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,7 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DEPLOY_OPENGL=1
-export DEPLOY_PIPEWIRE=1
+export DEPLOY_PULSE=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/Myth2_64bit /usr/lib/libopenal.so*
